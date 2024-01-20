ed45f338ff504e57deeb37d84121c19c15f59e43
M <tomasz.czartowski@gmail.com>
2024-01-20 18:16:25 +0100

    Add files via upload

---
+++ binary-board-master/rotm2quat.h
@@ -0,0 +1,9 @@
+/* sqrt example */
+#include <stdio.h>      /* printf */
+#include <math.h>       /* sqrt */
+
+float mRot2Quat(int what,
+	float r11, float r12, float r13,
+	float r21, float r22, float r23,
+	float r31, float r32, float r33
+);
